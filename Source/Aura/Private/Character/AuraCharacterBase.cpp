e30e9710562110c771c2f0f36bc58c5feba28b63
Rodrigo do Ó <26281264+Rodrigodoo@users.noreply.github.com>
2026-02-16 15:47:29 -0500

    Aura and Goblin Spear Bluprints setup

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -7,6 +7,10 @@ AAuraCharacterBase::AAuraCharacterBase()
 {
  	// No tick for this class
 	PrimaryActorTick.bCanEverTick = false;
+	
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
