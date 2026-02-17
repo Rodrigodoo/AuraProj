6ffce8a71b2611fb8c85e23022b41cb17c8d117e
Rodrigo do Ó <26281264+Rodrigodoo@users.noreply.github.com>
2026-02-17 17:11:20 -0500

    Implementing Highlight material using Custom Depth Stencil

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,18 +3,39 @@
 
 #include "Character/AuraEnemy.h"
 
+#include "Aura/Aura.h"
+
+
+AAuraEnemy::AAuraEnemy()
+{
+	// Set collision to block visibility
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility,ECR_Block);
+}
 
 void AAuraEnemy::HighlightActor()
 {
 	// Do highlight
+	// The material used needs a stencil value of 250 for highlight
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 
-	bHighlightActor = true;
+	// If this actor has a weapon, then Highlight it
+	if (Weapon)
+	{
+		Weapon->SetRenderCustomDepth(true);
+		Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	}
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
 	// Remove highlight
+	// The material used needs a stencil value of 250 for highlight
+	GetMesh()->SetRenderCustomDepth(false);
 
-	bHighlightActor = false;
-	
+	// If this actor has a weapon, then Unhighlight it
+	if (Weapon)
+	{
+		Weapon->SetRenderCustomDepth(false);
+	}
 }
--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -15,17 +15,15 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
 public:
+	AAuraEnemy();
+	
 	//---IEnemyInterface---Start
 	
 	// Highlights the enemy
-	virtual void HighlightActor();
+	virtual void HighlightActor() override;
 	
 	// Unhighlights the enemy
-	virtual void UnHighlightActor();
+	virtual void UnHighlightActor() override;
 	
 	//---IEnemyInterface---End
-
-	// Temp Bool flag
-	UPROPERTY(BlueprintReadOnly)
-	bool bHighlightActor = false;
 };
