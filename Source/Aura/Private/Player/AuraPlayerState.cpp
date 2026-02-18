efaf31d9d305a2ed6172f21e42e2d6233d4e29f2
Rodrigo do Ó <26281264+Rodrigodoo@users.noreply.github.com>
2026-02-18 12:30:59 -0500

    Fix NetUpdateFrequency deprecation. Replaced with  SetNetUpdateFrequency

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -8,5 +8,5 @@ AAuraPlayerState::AAuraPlayerState()
 	// Frequency that the server will send updates to clients
 	// Note: For games that the ASC is set on player state the frequency needs to be high to make sure replication 
 	// occurs at acceptable intervals. In games like Lyra or Fornite it's set around 100.0f
-	NetUpdateFrequency = 100.f;
+	SetNetUpdateFrequency(100.f);
 }
