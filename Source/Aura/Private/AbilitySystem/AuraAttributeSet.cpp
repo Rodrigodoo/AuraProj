0687ed3a93e2c9417ce016b8aeb3e9b4ed2ba1b1
Rodrigo do Ó <26281264+Rodrigodoo@users.noreply.github.com>
2026-02-19 09:46:32 -0500

    Creation of four Attributes: Health, maxHealth, Mana, MaxMana. Setup the replication for those attributes

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -3,3 +3,48 @@
 
 #include "AbilitySystem/AuraAttributeSet.h"
 
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
+UAuraAttributeSet::UAuraAttributeSet()
+{
+	
+}
+
+void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	// Registers the variables to be replicated
+	
+	// Without conditions
+	// Replicating everytime it's set (Opposite to REPNOTIFY_OnChange, which only replicates if value changed)
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	// Notifies the Gameplay Attribute System of the change on this attribute
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldHealth);
+}
+
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	// Notifies the Gameplay Attribute System of the change on this attribute
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	// Notifies the Gameplay Attribute System of the change on this attribute
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Mana, OldMana);
+}
+
+void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	// Notifies the Gameplay Attribute System of the change on this attribute
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxMana, OldMaxMana);
+}
--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -7,11 +7,53 @@
 #include "AuraAttributeSet.generated.h"
 
 /**
- * 
+ * Attribute Set in charge of holding attributes for the Aura project
  */
 UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UAuraAttributeSet();
+	// Replication method override for variable replication
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	//~ Begin - Vital  Attributes
+	
+	// Character's Health
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	
+	// Character's Max Health
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	
+	// Replication method for Health
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData & OldHealth) const;
+	
+	// Replication method for Max Health
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData & OldMaxHealth) const;
+	//~ End - Vital  Attributes
+	
+	// ~ Begin Consumable Attributes
+	
+	// Character's Mana
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Consumable Attributes")
+	FGameplayAttributeData Mana;
+	
+	// Character's Max Mana
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Consumable Attributes")
+	FGameplayAttributeData MaxMana;
+	
+	// Replication method for Mana
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData & OldMana) const;
+	
+	// Replication method for Max Mana
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData & OldMaxMana) const;
+	// ~ End Consumable Attributes
 };
