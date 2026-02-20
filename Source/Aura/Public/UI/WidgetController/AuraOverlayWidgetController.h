29aec16157c968df57e5407c15912903179981bd
Rodrigo do Ó <26281264+Rodrigodoo@users.noreply.github.com>
2026-02-20 10:37:16 -0500

    UAuraOverlayWidgetController fix pish

---
+++ Source/Aura/Public/UI/WidgetController/AuraOverlayWidgetController.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "AuraOverlayWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraOverlayWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+	
+};
