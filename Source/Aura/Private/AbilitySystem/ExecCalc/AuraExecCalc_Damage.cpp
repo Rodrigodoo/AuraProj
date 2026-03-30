02068818fde9537bcd63791d64f67e3f13279682
Rodrigo do Ó <26281264+Rodrigodoo@users.noreply.github.com>
2026-03-30 12:05:12 -0500

    Added a placeholder claculation for the AuraExecCalc_Damage.cpp to check if it was being called and calculated corretly. For now only Armor is being captured

--- Source/Aura/Private/AbilitySystem/ExecCalc/AuraExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/AuraExecCalc_Damage.cpp
@@ -4,9 +4,30 @@
 #include "AbilitySystem/ExecCalc/AuraExecCalc_Damage.h"
 
 #include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+// Internal struct to store Attribute Capture Definitions
+struct AuraDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	AuraDamageStatics()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+	}
+};
+
+// Get the Damage Statics object
+static const AuraDamageStatics& DamageStatics()
+{
+	// It will always be the same object
+	static AuraDamageStatics DStatics;
+	return DStatics;
+}
 
 UAuraExecCalc_Damage::UAuraExecCalc_Damage()
 {
+	// Add Attributes to Capture from the Damage Statics
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 }
 
 void UAuraExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -18,5 +39,26 @@ void UAuraExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExe
 	const AActor* SourceAvatar = SourceAbilitySystemComponent ? SourceAbilitySystemComponent->GetAvatarActor() : nullptr;
 	const AActor* TargetAvatar = TargetAbilitySystemComponent ? TargetAbilitySystemComponent->GetAvatarActor() : nullptr;
 	
+	// Get the Effect Spect
 	const FGameplayEffectSpec& EffectSpec = ExecutionParams.GetOwningSpec();
+	
+	// Gather tags from source and target
+	const FGameplayTagContainer* SourceTag = EffectSpec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTag = EffectSpec.CapturedTargetTags.GetAggregatedTags();
+	
+	// Capture attribute's magnitude (value)
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTag;
+	EvaluationParameters.TargetTags = TargetTag;
+	
+	// Calculate the Captured Attributes' magnitude
+	float ArmorMagnitude = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluationParameters,ArmorMagnitude);
+	ArmorMagnitude = FMath::Max(0.f, ArmorMagnitude);
+	++ArmorMagnitude;
+	
+	// Build the Execution Output
+	// Add any output modifier that need change
+	const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, ArmorMagnitude);
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
